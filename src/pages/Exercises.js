71899537b2063062b35d7aba6b7f3046db95a831
srirachy <caseyereyes@gmail.com>
2023-12-24 16:32:51 -0800

    add initial code

---
+++ src/pages/Exercises.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Exercises = () => {
+    return ( 
+        <>
+            <h1>
+                Hello, please use Navigation Bar to direct to exercises.
+            </h1>
+        </>
+     );
+}
+ 
+export default Exercises;
